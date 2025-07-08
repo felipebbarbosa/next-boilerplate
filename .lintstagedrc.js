bfbb49cb40dd590aa4957a91e3bd224965996c0e
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2025-07-07 21:41:37 -0300

    chore: initial jest tests

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,7 @@ module.exports = {
   // lint & prettify ts and js files
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `npm run lint --fix . ${filenames.join(' --file')}`
+    `npm run lint --fix . ${filenames.join(' --file')}`,
+    `npm test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 }
